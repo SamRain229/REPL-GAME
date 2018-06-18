170cb6727236e4c7c4da36c1314985b141f0d2e9
Joshua Meadows <joshuakmeadows@gmail.com>
2018-06-18 18:57:37 +0000

    Some changes to the code

--- REPLGame_0.1.rb
+++ REPLGame_0.1.rb
@@ -8,28 +8,21 @@ option = gets.chomp.downcase.strip
            puts "you see paths leading Left and Right"
        when "no"
            puts "you are eaten by a monster"
-    else
-        puts "Yes or No"
     end
 
 direction = gets.chomp
     case direction
         when "Left"
-            puts "You enter a room with three chests, which do you open, 1 2 or 3"
+            puts "You enter a room with a chest in it, do you open it?"
         when "Right"
             puts "you fall into a hole"
-        else
-            puts "you run into a wall"
     end
 
 chest = gets.chomp.downcase.strip
     case chest
-        when "1"
-            puts "you find 300 gold coins"
-        when "2"
-            puts "you find a cool sword"
-        when "3"
-            puts "You find a map"
-    
+        when "yes"
+        puts "you find 300 gold and a map"
+        when "no"
+        puts "you wander in the labrynth until you pass out"
     end
-puts "End of Game"
+puts "Thank you for playing my game"
